2c7860014d3408c82cc2cc5fc8e0f731cd8735c1
MiMiSoe <mimisoe@acedatasystems.com>
2024-04-27 23:01:58 +0630

    RestApi [CRUD]

---
+++ MMSDotNetCore.RestApi/ConnectionStrings.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace MMSDotNetCore.RestApi;
+
+internal static class ConnectionStrings
+{
+    public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+    {
+        DataSource = ".",
+        InitialCatalog = "DotNetTrainingBatch4",
+        UserID = "sa",
+        Password = "sa@123",
+        TrustServerCertificate = true
+    };
+}
---
+++ MMSDotNetCore.RestApi/Db/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MMSDotNetCore.RestApi.Models;
+
+namespace MMSDotNetCore.RestApi.Db
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
