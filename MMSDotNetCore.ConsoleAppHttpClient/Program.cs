035d7e686e6e7e8def3c07d8eb586021e248d660
MiMiSoe <mimisoe968@gmail.com>
2024-05-11 17:43:52 +0630

    [Complete] RestApi With JsonData

---
+++ MMSDotNetCore.ConsoleAppHttpClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+string jsonStr = await File.ReadAllTextAsync("IncompatibleFood.json");
+MainDto data = JsonConvert.DeserializeObject<MainDto>(jsonStr);
+Console.WriteLine(jsonStr);
+foreach (var item in data.Tbl_IncompatibleFood)
+{
+    Console.WriteLine(item.FoodA + " + " + item.FoodB + "=>" + item.Description);
+}
+Console.ReadKey();
+
+
+public class MainDto
+{
+    public Tbl_Incompatiblefood[] Tbl_IncompatibleFood { get; set; }
+}
+
+public class Tbl_Incompatiblefood
+{
+    public int Id { get; set; }
+    public string FoodA { get; set; }
+    public string FoodB { get; set; }
+    public string Description { get; set; }
+}
---
+++ MMSDotNetCore.RestApiWithNLayer/Features/IncompatibleFood/IncompatibleFoodController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MMSDotNetCore.RestApiWithNLayer.Features.IncompatibleFood
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncompatibleFoodController : ControllerBase
+    {
+        private readonly IncompatibleFood _data;
+        public IncompatibleFoodController()
+        {
+
+        }
+
+        private async Task<IncompatibleFood> GetDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("IncompatibleFood.json");
+            IncompatibleFood data = JsonConvert.DeserializeObject<IncompatibleFood>(jsonStr);
+            return data;
+        }
+
+        [HttpGet("IncompatibleFood")]
+        public async Task<IActionResult> GetIncompatibleFood()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.Tbl_IncompatibleFood);
+        }
+
+        [HttpGet("Description")]
+        public async Task<IActionResult> GetDescription()
+        {
+            var model = await GetDataAsync();
+            var distinctDescriptions = model.Tbl_IncompatibleFood
+                               .Select(x => x.Description)
+                               .Distinct()
+                               .ToList();
+            return Ok(distinctDescriptions);
+        }
+
+        [HttpGet("{description}")]
+        public async Task<IActionResult> GetDescription(string description)
+        {
+            var model = await GetDataAsync();
+            var lst = model.Tbl_IncompatibleFood.Where(x => x.Description == description)
+                               .ToList();
+            return Ok(lst);
+        }
+
+        [HttpGet("{foodA}/foodB")]
+        public async Task<IActionResult> GetDescription(string foodA, string foodB)
+        {
+            var model = await GetDataAsync();
+            var data = model.Tbl_IncompatibleFood.FirstOrDefault(x => x.FoodA == foodA && x.FoodB == foodB);
+            return Ok(data.Description);
+        }
+    }
+
+    public class IncompatibleFood
+    {
+        public Tbl_Incompatiblefood[] Tbl_IncompatibleFood { get; set; }
+    }
+
+    public class Tbl_Incompatiblefood
+    {
+        public int Id { get; set; }
+        public string FoodA { get; set; }
+        public string FoodB { get; set; }
+        public string Description { get; set; }
+    }
+}
